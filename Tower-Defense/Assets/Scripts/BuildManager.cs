d87ece61e35c72ec589168e2d3dca7c0cc0940fe
DESKTOP\David <Anderson.David@protonmail.com>
2018-03-09 13:47:56 -0600

    Added Turret Build On Node Click
    Created BuildManager to track turret type.
    Created Node script to change color on hover and build selected turret on click if no turret built.

---
+++ Tower-Defense/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("More than one Build Manager!");
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject getTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Tower-Defense/Assets/Scripts/Node.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    private Color startColor;
+    public Vector3 turretOffset;
+    private Renderer rend;
+
+    private GameObject turret;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Already Built");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.getTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + turretOffset, transform.rotation);
+
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+	
+}
