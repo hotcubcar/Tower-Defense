81c091934f1e29a302379ab3e93f0ccbd845ab0c
DESKTOP\David <Anderson.David@protonmail.com>
2018-03-11 14:52:25 -0500

    Explosion Range & Impact Effect
    Added Range to Missile Explosion.
    Added light and particles to Explosion effect.

--- Tower-Defense/Assets/Scripts/Bullet.cs
+++ Tower-Defense/Assets/Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour {
     private Transform target;
 
     public float speed = 70;
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
 
     public void Seek(Transform _target)
@@ -37,6 +38,7 @@ public class Bullet : MonoBehaviour {
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
 
 
 	}
@@ -44,8 +46,40 @@ public class Bullet : MonoBehaviour {
     void HitTarget()
     {
         GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effectIns, 3f);
+        Destroy(effectIns, 5f);
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
         Destroy(gameObject);
-        Destroy(target.gameObject);
+    }
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            //Debug.Log("Explode check on "  + collider.tag);
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+    void Damage(Transform enemy)
+    {
+        //Debug.Log("Destroy check on " + enemy.name);
+        enemy = enemy.parent.parent;
+
+        Destroy(enemy.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
